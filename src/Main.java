5a2c815aefed2470880d86722fff215dde033ffe
Mateus Rossi <rossijf2@gmail.com>
2022-07-20 09:31:18 -0300

    Abstracao de curso e mentoria

---
+++ src/Main.java
@@ -0,0 +1,24 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.sql.SQLOutput;
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso = new Curso();
+        curso.setTitulo("Curso Java");
+        curso.setDescricao("Descricao curso Java");
+        curso.setCargaHoraria(50);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Mentoria ministrada para o curso de Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso);
+        System.out.println(mentoria);
+
+    }
+}
---
+++ src/br/com/dio/desafio/dominio/Curso.java
@@ -0,0 +1,43 @@
+package br.com.dio.desafio.dominio;
+public class Curso {
+
+    private String titulo;
+    private String descricao;
+    int cargaHoraria;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public Curso setTitulo(String titulo) {
+        this.titulo = titulo;
+        return this;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public Curso setDescricao(String descricao) {
+        this.descricao = descricao;
+        return this;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public Curso setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" +
+                "titulo='" + titulo + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", cargaHoraria=" + cargaHoraria +
+                '}';
+    }
+}
